054017408e1d865ff6eb1c26556ec8a1ae9bfd8f
Abhijeet <abhijeetkumar47244@gmail.com>
2024-06-02 11:25:16 +0530

    Level 2 Android

---
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+
+    }
+}
+
